0249ef0800981b39387995ed7a4986b6e9074166
Aseef Ahmed <aseefahmed@gmail.com>
2018-05-05 16:43:41 +0600

    =

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,7 @@ var upload = multer({ dest: './public/img/uploads' })
 router.use(flash());
 
 router.get('/dashboard', ensureAuthenticated, function(req, res, next) {
+	res.send('done');
   let posts = db.get('posts');
   let total_posts = posts.count({},{}, function(err,count_posts){
 
